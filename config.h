5d0fe0b8ed59452c14b56846628359274db057e3
Osama <iosamaify@gmail.com>
2021-07-27 15:48:48 +0200

    apply swallow patch

--- config.h
+++ config.h
@@ -3,24 +3,25 @@
 #include "movestack.c"
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx = 1;        /* border pixel of windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+static const unsigned int gappih   = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv   = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh   = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov   = 20;       /* vert outer gap between windows and screen edge */
+static       int smartgaps         = 1;        /* 1 means no outer gap when there is only one window */
+static const int showbar           = 1;        /* 0 means no bar */
+static const int topbar            = 1;        /* 0 means bottom bar */
+static const int swallowfloating   = 0;        /* 1 means swallow floating windows by default */
+static const int focusonwheel      = 0;
+static const char *fonts[]         = { "monospace:size=10" };
+static const char dmenufont[]      = "monospace:size=10";
+static const char col_gray1[]      = "#222222";
+static const char col_gray2[]      = "#444444";
+static const char col_gray3[]      = "#bbbbbb";
+static const char col_gray4[]      = "#eeeeee";
+static const char col_cyan[]       = "#005577";
+static const char *colors[][3]     = {
 	/*                fg         bg         border   */
 	[SchemeNorm]  = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]   = { col_gray4, col_cyan,  col_cyan  },
@@ -53,11 +54,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,        NULL,       0,            1,           -1 },
-	//{ "Firefox",  NULL,        NULL,       1 << 8,       0,           -1 },
-	//{ "Chromium", NULL,        NULL,       1 << 8,       0,           -1 },
-	{ NULL,       "spterm",    NULL,       SPTAG(0),     1,           -1 },
+	/* class     instance     title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,        NULL,           0,         1,          0,           0,        -1 },
+	{ "St",      NULL,        NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester",    0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,      "spterm",    NULL,           SPTAG(0),  1,          1,           0,        -1 },
 };
 
 /* layout(s) */
