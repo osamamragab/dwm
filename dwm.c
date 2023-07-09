c0c95f847ac6245af235b8ca5845ecc1e96e377c
Osama Ragab <theosamaragab@gmail.com>
2023-07-09 22:43:53 +0300

    update monocle layout with gaps

--- dwm.c
+++ dwm.c
@@ -1317,14 +1317,14 @@ monocle(Monitor *m)
 {
 	unsigned int n = 0;
 	Client *c;
+	int oh, ov, ih, iv;
 
-	for (c = m->clients; c; c = c->next)
-		if (ISVISIBLE(c))
-			n++;
+	getgaps(m, &oh, &ov, &ih, &iv, &n);
+
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		resize(c, m->wx+ov, m->wy+oh, m->ww-(c->bw*2)-(ov*2), m->wh-(c->bw*2)-(oh*2), 0);
 }
 
 void
