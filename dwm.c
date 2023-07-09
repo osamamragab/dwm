551d5cd3877aa01a98d49544840049e18e346407
Osama Ragab <theosamaragab@gmail.com>
2023-07-10 00:00:41 +0300

    fix swallow condition

--- dwm.c
+++ dwm.c
@@ -524,9 +524,7 @@ void
 swallow(Client *p, Client *c)
 {
 
-	if (c->noswallow || c->isterminal)
-		return;
-	if (c->noswallow && !swallowfloating && c->isfloating)
+	if (c->noswallow || c->isterminal || (!swallowfloating && c->isfloating))
 		return;
 
 	detach(c);
