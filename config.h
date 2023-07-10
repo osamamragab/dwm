6a7bbf2d2213238d7dfaf97ede81990b9116b138
Osama Ragab <theosamaragab@gmail.com>
2023-07-10 15:10:42 +0300

    set borderpx to 2

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 #define BROWSER "librewolf"
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
+static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static unsigned int gappiv    = 10;       /* vert inner gap between windows */
