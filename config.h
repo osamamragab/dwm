e46eb6bdb1b0e7db0d45fb46b3cdcffb0e751543
Osama Ragab <theosamaragab@gmail.com>
2023-09-05 20:34:57 +0300

    Revert "apply swallow patch"
    This reverts commit 92c9a7f824017665f428f27d93f0914a83dd9c0a.

--- config.h
+++ config.h
@@ -6,7 +6,6 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char font[]            = "monospace:size=10";
@@ -39,15 +38,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class         instance     title           tags mask  isfloating  isterminal   noswallow  monitor */
-	{ "Gimp",        NULL,        NULL,           0,         1,          0,           0,         -1 },
-	{ "Dragon",      NULL,        NULL,           0,         1,          0,           1,         -1 },
-	{ TERMCLASS,     NULL,        NULL,           0,         0,          1,           0,         -1 },
-	{ TERMCLASS,     "floatterm", NULL,           0,         1,          0,           0,         -1 },
-	{ NULL,          NULL,        "Event Tester", 0,         0,          0,           1,         -1 }, /* xev */
-	{ TERMCLASS,     "spterm",    NULL,           SPTAG(0),  1,          1,           0,         -1 },
-	{ TERMCLASS,     "spnote",    NULL,           SPTAG(1),  1,          1,           0,         -1 },
-	{ TERMCLASS,     "spcalc",    NULL,           SPTAG(2),  1,          1,           0,         -1 },
+	/* class         instance     title           tags mask  isfloating  monitor */
+	{ "Gimp",        NULL,        NULL,           0,         1,          -1 },
+	{ "Dragon",      NULL,        NULL,           0,         1,          -1 },
+	{ TERMCLASS,     NULL,        NULL,           0,         0,          -1 },
+	{ TERMCLASS,     "floatterm", NULL,           0,         1,          -1 },
+	{ NULL,          NULL,        "Event Tester", 0,         0,          -1 }, /* xev */
+	{ TERMCLASS,     "spterm",    NULL,           SPTAG(0),  1,          -1 },
+	{ TERMCLASS,     "spnote",    NULL,           SPTAG(1),  1,          -1 },
+	{ TERMCLASS,     "spcalc",    NULL,           SPTAG(2),  1,          -1 },
 };
 
 /* layout(s) */
